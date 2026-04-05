4b1ca1f41214282fea4634b5c0337d77888b4de5
MumaSiaga <mumandafhadzi@gmail.com>
2026-04-05 11:25:09 +0200

    test: mock IntersectionObserver in Jest setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,25 @@
-import '@testing-library/jest-dom'
+class MockIntersectionObserver {
+  constructor(callback, options) {
+    this.callback = callback;
+    this.options = options;
+  }
+
+  observe() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+
+  disconnect() {
+    return null;
+  }
+
+  takeRecords() {
+    return [];
+  }
+}
+
+global.IntersectionObserver = MockIntersectionObserver;
+window.IntersectionObserver = MockIntersectionObserver;
